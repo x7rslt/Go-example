1c3fd766052553293033bf3b537060b5fd8e4ab7
x7rslt <1660019216@qq.com>
2021-02-07 23:13:48 +0800

    sync.WaitGroup

---
+++ waitgroups/exercise/maing.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
